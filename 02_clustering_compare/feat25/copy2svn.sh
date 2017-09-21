53c77256088d14df537607694b08929464dbd9b0
Leiming Yu <leimingyu830@gmail.com>
2017-09-21 17:25:38 -0400

    up runtime benchmarks

---
+++ 02_clustering_compare/feat25/copy2svn.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#cp /home/leiming/git/magus/02_clustering_compare/feat25/*.pdf .
+#cp /home/leiming/git/magus/02_clustering_compare/feat25/*.png .
+
+cp *.pdf ~/Dropbox/09_myPaper/08_ispass2018/paper_svn/leiming_ispass18/Figs/sim_eval/
+cp *.png ~/Dropbox/09_myPaper/08_ispass2018/paper_svn/leiming_ispass18/Figs/sim_eval/
