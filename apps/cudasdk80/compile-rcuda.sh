71c73a2ab7c05a1cc08fe77afbdcb15d077b2433
Leiming Yu <leimingyu830@gmail.com>
2017-10-07 15:25:59 -0400

    compile rcuda for cudasdk

---
+++ apps/cudasdk80/compile-rcuda.sh
@@ -0,0 +1,6 @@
+#/bin/bash
+cd 0_Simple && ./0_compile_rcuda.sh
+cd 3_Imaging && ./0_compile_rcuda.sh
+cd 4_Finance && ./0_compile_rcuda.sh
+cd 6_Advanced && ./0_compile_rcuda.sh
+cd 7_CUDALibraries && ./0_compile_rcuda.sh
