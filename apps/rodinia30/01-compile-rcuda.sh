00feb4bfec9090aadec229bc354e712a84520412
Leiming Yu <leimingyu830@gmail.com>
2017-10-07 21:37:18 -0400

    update

--- apps/rodinia30/01-compile-rcuda.sh
+++ apps/rodinia30/01-compile-rcuda.sh
@@ -7,11 +7,18 @@ do
 			[ "$currDir" != "cfd" ] && \
 			[ "$currDir" != "hybridsort" ] && \
 			[ "$currDir" != "mummergpu" ] && \
+			[ "$currDir" != "kmeans" ] && \
 			[ "$currDir" != "nn" ] && \
+			[ "$currDir" != "_nn" ] && \
+			[ "$currDir" != "leukocyte" ] && \
+			[ "$currDir" != "myocyte" ] && \
+			[ "$currDir" != "common" ] && \
+			[ "$currDir" != "data" ] && \
 			[ "$currDir" != "particlefilter" ] && \
 			[ "$currDir" != "srad" ] && \
 			[ "$currDir" != "streamcluster" ]; then
 
+			#echo "$currDir"
 			cd $currDir
 			make clean && make EXTRA_NVCCFLAGS=--cudart=shared
 			cd ..
