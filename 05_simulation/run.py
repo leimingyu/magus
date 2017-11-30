2a3225e311a212d080e812504395d4c8adc00910
Leiming Yu <leimingyu830@gmail.com>
2017-11-30 06:35:11 -0500

    update run

--- 05_simulation/run.py
+++ 05_simulation/run.py
@@ -16,7 +16,9 @@ DEVNULL = open(os.devnull, 'wb', 0) # no std out
 LOGT = True # timing log
 
 class cd:
-    """Context manager for changing the current working directory"""
+    """
+    Context manager for changing the current working directory
+    """
     def __init__(self, newPath):
         self.newPath = os.path.expanduser(newPath)
 
@@ -27,29 +29,9 @@ class cd:
     def __exit__(self, etype, value, traceback):
         os.chdir(self.savedPath)
 
-
-def run_remote_mp(DEVNULL, app_dir, app_cmd, *args):
-    start = time.time()
-    with cd(app_dir):
-        #print os.getcwd()
-        if len(args) == 0:
-            # directly run the command 
-            check_call([app_cmd], stdout=DEVNULL, stderr=STDOUT)
-        else:
-            app_cmd_list = [app_cmd]
-            for arg in args:
-                app_cmd_list.append(arg)
-            #concatenate the cmds into one string (including env var) 
-            cmd_str = ' '.join(str(e) for e in app_cmd_list)
-            check_call(cmd_str, stdout=DEVNULL, stderr=STDOUT, shell=True)
-
-    end = time.time()
-    print("{} to {} = {:.3f} seconds".format(start, end, end - start))
-
-
 def run_mp(app_dir, app_cmd):
     """
-    run multiprocessing
+    Run multiprocessing
     """
     start = time.time()
     with cd(app_dir):
@@ -67,6 +49,7 @@ def run_remote(app_dir, *args):
     arg_num = len(args)
     if arg_num > 0:
         cmd_str = ' '.join(str(e) for e in args)
+        #multiprocessing.log_to_stderr(logging.DEBUG)
         p = mp.Process(target=run_mp, args=(app_dir, cmd_str))
         p.start()
 
@@ -79,41 +62,19 @@ def run_remote(app_dir, *args):
 # tests
 #------------------------------------------------------------------------------
 def test1_run2():
-    p = mp.Process(target=run_remote_mp, args=(std_out,
-        '../apps/rcuda_cusdk80/0_Simple/matrixMul/',
-        './matrixMul'))
-    p.start()
-
-    p = mp.Process(target=run_remote_mp, args=(std_out,
-        '../apps/rcuda_cusdk80/0_Simple/vectorAdd/',
-        './vectorAdd'))
-    p.start()
+    run_remote('../apps/rcuda_cusdk80/0_Simple/matrixMul/', './matrixMul')
+    run_remote('../apps/rcuda_cusdk80/0_Simple/vectorAdd/', './vectorAdd')
 
 def test2_selDev():
-    #multiprocessing.log_to_stderr(logging.DEBUG)
-    p = mp.Process(target=run_remote_mp, args=(std_out,
-        '../apps/rcuda_cusdk80/0_Simple/matrixMul/',
-        'RCUDA_DEVICE_0=mcx1.coe.neu.edu:0',
-        './matrixMul'))
-    p.start()
-    p = mp.Process(target=run_remote_mp, args=(std_out,
-        '../apps/rcuda_cusdk80/0_Simple/vectorAdd/',
-        'RCUDA_DEVICE_0=mcx1.coe.neu.edu:1',
-        './vectorAdd'))
-    p.start()
-
-def test3():
-    #run_remote('../apps/rcuda_cusdk80/0_Simple/vectorAdd/', './vectorAdd')
-
-    #run_remote('../apps/rcuda_cusdk80/0_Simple/vectorAdd/', 
-    #        'RCUDA_DEVICE_0=mcx1.coe.neu.edu:0',
-    #        './vectorAdd')
+    run_remote('../apps/rcuda_cusdk80/0_Simple/matrixMul/', 
+            'RCUDA_DEVICE_0=mcx1.coe.neu.edu:0', './matrixMul')
+    run_remote('../apps/rcuda_cusdk80/0_Simple/vectorAdd/', 
+            'RCUDA_DEVICE_0=mcx1.coe.neu.edu:1', './vectorAdd')
 
 
 def tests():
-    #test1_run2()
+    test1_run2()
     #test2_selDev()
-    test3()
 
 
 
