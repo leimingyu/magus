dc5236927000973fcc087189b54f7ec777bad41b
Leiming Yu <leimingyu830@gmail.com>
2017-12-22 11:42:11 -0500

    profiling script

---
+++ 00_featSel/13cudasdk_native/profile_app.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+LD_LIBRARY_PATH="/usr/local/cuda/lib64"
+export LD_LIBRARY_PATH
+
+appDir=$1
+appExe=$2
+targetMetric=$3
+
+#echo $appDir
+#echo $appExe
+#echo $targetMetric
+
+out_metrics=$appExe"_metrics.csv"
+#echo $out_metrics
+
+cd $appDir && \
+make clean && \
+make && nvprof --metrics $targetMetric  --csv --log-file $out_metrics $appExe
