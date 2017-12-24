5c4caec53a338802ddaa6afb01113649a63f8ae3
Leiming Yu <leimingyu830@gmail.com>
2017-12-24 14:37:34 -0500

    profile

--- 00_featSel/cuda_apps/02_profile.sh
+++ 00_featSel/cuda_apps/02_profile.sh
@@ -4,12 +4,12 @@ for currDir in *
 do
   if [ -d $currDir ];then
     if [ "$currDir" != "common" ]; then
-			cd $currDir
 			echo $currDir
 			logFile=$currDir"_metrics.csv"
 			#appExe=$currDir
 			echo $logFile
 			echo $appExe
+			cd $currDir
 			/usr/local/cuda-8.0/bin/nvprof --metrics all --csv --log-file $logFile ./$appExe
 			mv *.csv ../
 			cd ..
