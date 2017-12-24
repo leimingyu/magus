5949cdbc571d1cea794af91bb50be069eaab1351
Leiming Yu <leimingyu830@gmail.com>
2017-12-24 14:21:27 -0500

    44 cuda apps

---
+++ 00_featSel/cuda_apps/02_prifile.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+for currDir in *
+do
+  if [ -d $currDir ];then
+    if [ "$currDir" != "common" ]; then
+			cd $currDir
+			echo $currDir
+			logFile=$currDir"_metrics.csv"
+			#appExe=$currDir
+			echo $logFile
+			echo $appExe
+			nvprof --metrics all --csv --log-file $logFile ./$appExe
+			mv *.csv ../
+			cd ..
+		fi
+  fi
+done
+
