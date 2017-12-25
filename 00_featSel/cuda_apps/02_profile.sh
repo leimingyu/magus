3e1b78bf3bf5d62d66093da21cd86b58b0772f3e
Leiming Yu <leimingyu830@gmail.com>
2017-12-25 11:11:36 -0500

    profiling other cuda apps

--- 00_featSel/cuda_apps/02_profile.sh
+++ 00_featSel/cuda_apps/02_profile.sh
@@ -4,17 +4,18 @@ for currDir in *
 do
   if [ -d $currDir ];then
     if [ "$currDir" != "common" ]; then
+			cd $currDir
 			echo $currDir
+
 			logFile=$currDir"_metrics.csv"
-			#appExe=$currDir
+			appExe=$currDir
+
 			echo $logFile
 			echo $appExe
-			cd $currDir
 			/usr/local/cuda-8.0/bin/nvprof --metrics all --csv --log-file $logFile ./$appExe
 			mv *.csv ../
 			cd ..
 		fi
   fi
-  break
 done
 
