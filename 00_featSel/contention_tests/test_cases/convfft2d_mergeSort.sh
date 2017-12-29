a9dbd42ef2f54a7e1ef79f426823faa6d5d4ab78
Leiming Yu <leimingyu830@gmail.com>
2017-12-28 21:33:09 -0500

    run2 tests

---
+++ 00_featSel/contention_tests/test_cases/convfft2d_mergeSort.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+app1="convfft2d"
+app1_keyword="convolutionFFT2D"
+app1_cmd="../../13apps/convolutionFFT2D/convolutionFFT2D"
+
+app2="mergeSort"
+app2_keyword="mergeSort"
+app2_cmd="../13apps/mergeSort/mergeSort"
+
+ITER=2
+
+if [ -f time_log ]
+then
+  rm time_log
+fi
+touch time_log
+
+for (( i=1; i<=$ITER; i++ ))
+do
+  ../timeApp.sh ${app1_cmd} &
+  ../timeApp.sh ${app2_cmd} &
+  wait
+done
+
+getAvg()
+{
+local keyWord=$1
+local runtime=$(awk -v pat="$keyWord"  '$0~pat{print}' time_log  | awk -F ":" '{print $2}' | awk '{ total += $1 } END { print total/NR }')
+echo $runtime
+}
+
+testName=$app1"_"$app2
+
+app1_runtime=$(getAvg $app1_keyword)
+app2_runtime=$(getAvg $app2_keyword)
+
+echo -e $testName": \t"$app1": "$app1_runtime"\t"$app2": "$app2_runtime
