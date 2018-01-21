cfe4fbb75d4824dc793a82956268f23c16ab8836
Leiming Yu <leimingyu830@gmail.com>
2018-01-21 11:59:59 -0500

    profile

--- 01_phase_contention/test_apps/02_profile.sh
+++ 01_phase_contention/test_apps/02_profile.sh
@@ -1,21 +1,31 @@
 #!/bin/bash
 
-for currDir in *
-do
-  if [ -d $currDir ];then
-    if [ "$currDir" != "common" ]; then
-			cd $currDir
-			echo $currDir
+cd batchCUBLAS && nvprof --metrics all --csv --log-file "batchCUBLAS_metrics.csv"  ./batchCUBLAS
+cd ../
 
-			logFile=$currDir"_metrics.csv"
-			appExe=$currDir
+cd BlackScholes && nvprof --metrics all --csv --log-file "BlackScholes_metrics.csv"  ./BlackScholes
+cd ../
 
-			echo $logFile
-			echo $appExe
-			/usr/local/cuda-8.0/bin/nvprof --metrics all --csv --log-file $logFile ./$appExe
-			mv *.csv ../
-			cd ..
-		fi
-  fi
-done
+cd boxFilterNPP && nvprof --metrics all --csv --log-file "boxFilterNPP_metrics.csv"  ./boxFilterNPP
+cd ../
 
+cd conjugateGradient && nvprof --metrics all --csv --log-file "conjugateGradient_metrics.csv"  ./conjugateGradient
+cd ../
+
+cd convolutionSeparable && nvprof --metrics all --csv --log-file "convolutionSeparable_metrics.csv"  ./convolutionSeparable
+cd ../
+
+cd dct8x8 && nvprof --metrics all --csv --log-file "dct8x8_metrics.csv"  ./dct8x8
+cd ../
+
+cd FDTD3d && nvprof --metrics all --csv --log-file "FDTD3d_metrics.csv"  ./FDTD3d
+cd ../
+
+cd histogram && nvprof --metrics all --csv --log-file "histogram_metrics.csv"  ./histogram
+cd ../
+
+cd nvgraph_Pagerank && nvprof --metrics all --csv --log-file "nvgraph_Pagerank_metrics.csv"  ./nvgraph_Pagerank
+cd ../
+
+cd simpleCUFFT_callback && nvprof --metrics all --csv --log-file "simpleCUFFT_callback_metrics.csv"  ./simpleCUFFT_callback
+cd ../
