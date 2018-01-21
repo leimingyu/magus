9eff83c2117b477a4ebf3c12cce8212a04427b17
Leiming Yu <leimingyu830@gmail.com>
2018-01-21 11:09:20 -0500

    test apps

---
+++ 01_phase_contention/test_apps/02_profile.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+for currDir in *
+do
+  if [ -d $currDir ];then
+    if [ "$currDir" != "common" ]; then
+			cd $currDir
+			echo $currDir
+
+			logFile=$currDir"_metrics.csv"
+			appExe=$currDir
+
+			echo $logFile
+			echo $appExe
+			/usr/local/cuda-8.0/bin/nvprof --metrics all --csv --log-file $logFile ./$appExe
+			mv *.csv ../
+			cd ..
+		fi
+  fi
+done
+
---
+++ 01_phase_contention/test_apps/03_clean.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+for currDir in *
+do
+  if [ -d $currDir ];then
+    if [ "$currDir" != "common" ]; then
+			cd $currDir
+			make clean
+			cd ..
+		fi
+  fi
+done
+
