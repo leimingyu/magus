dc2bd6c2b310e9d46bb819d747e497bcfabee30e
Leiming Yu <leimingyu830@gmail.com>
2018-01-21 14:05:13 -0500

    update sh

--- 01_phase_contention/test_apps/04_moveMetrics.sh
+++ 01_phase_contention/test_apps/04_moveMetrics.sh
@@ -1,2 +1,15 @@
 #!/bin/bash
-mv *.csv  ../metrics
+#mv *.csv  ../metrics
+
+for currDir in *
+do
+  if [ -d $currDir ];then
+    if [ "$currDir" != "common" ]; then
+			cd $currDir
+			mv *_metrics.csv ../../test_apps_metrics
+			mv *_trace.csv ../../test_apps_trace
+			cd ..
+		fi
+  fi
+done
+
