20010da71676f8bae565b9c17685d373040439d1
Leiming Yu <leimingyu830@gmail.com>
2018-01-21 21:33:38 -0500

    test for single execution

---
+++ 01_phase_contention/test_apps_ContentionTest/test_single/BlackScholes.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+app1="BlackScholes"
+app1_keyword="BlackScholes"
+app1_cmd="../../test_apps/BlackScholes/BlackScholes"
+
+ITER=100
+
+if [ -f time_log ]
+then
+  rm time_log
+fi
+touch time_log
+
+for (( i=1; i<=$ITER; i++ ))
+do
+  ../timeApp.sh ${app1_cmd} &
+  wait
+done
+
+getAvg()
+{
+local keyWord=$1
+local runtime=$(awk -v pat="$keyWord"  '$0~pat{print}' time_log  | awk -F ":" '{print $2}' | awk '{ total += $1 } END { print total/NR }')
+echo $runtime
+}
+
+testName=$app1
+
+app1_runtime=$(getAvg $app1_keyword)
+
+echo -e $testName": \t"$app1": "$app1_runtime
