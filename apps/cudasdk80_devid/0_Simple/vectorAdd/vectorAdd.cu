03fdee4818d3228fc0445267aeda64b77f5098ea
Leiming Yu <leimingyu830@gmail.com>
2018-02-23 13:01:00 -0500

    update vectoradd

--- apps/cudasdk80_devid/0_Simple/vectorAdd/vectorAdd.cu
+++ apps/cudasdk80_devid/0_Simple/vectorAdd/vectorAdd.cu
@@ -44,8 +44,31 @@ vectorAdd(const float *A, const float *B, float *C, int numElements)
  * Host main routine
  */
 int
-main(void)
+main(int argc, char **argv)
 {
+		int devID = 0;
+		if(argc == 2) {
+				devID = atoi(argv[1]);
+		}
+		printf("select device : %d\n", devID);
+		cudaSetDevice(devID);
+
+		cudaError_t error;
+		cudaDeviceProp deviceProp;
+
+		error = cudaGetDeviceProperties(&deviceProp, devID);
+		if (error != cudaSuccess)
+		{
+				printf("cudaGetDeviceProperties returned error %s (code %d), line(%d)\n", cudaGetErrorString(error), error, __LINE__);
+		}
+		else
+		{
+				printf("GPU Device %d: \"%s\" with compute capability %d.%d\n\n", devID, deviceProp.name, deviceProp.major, deviceProp.minor);
+		}
+
+
+
+
     // Error code to check return values for CUDA calls
     cudaError_t err = cudaSuccess;
 
