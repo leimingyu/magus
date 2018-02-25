9955b0eb63aedf50ab6f9295a47c1fb03ba0fa5d
Leiming Yu <leimingyu830@gmail.com>
2018-02-24 21:17:03 -0500

    update sgemm

--- apps/devid_parboil/benchmarks/sgemm/run.sh
+++ apps/devid_parboil/benchmarks/sgemm/run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-./sgemm  -i ../../datasets/sgemm/small/input/matrix1.txt,../../datasets/sgemm/small/input/matrix2.txt,../../datasets/sgemm/small/input/matrix2t.txt
+./sgemm  -i ../../datasets/sgemm/small/input/matrix1.txt,../../datasets/sgemm/small/input/matrix2.txt,../../datasets/sgemm/small/input/matrix2t.txt -d $1
