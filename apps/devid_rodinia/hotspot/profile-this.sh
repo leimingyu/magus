f8cb3da32170c30df4cf6bbdf5b12046d9771306
Leiming Yu <leimingyu830@gmail.com>
2018-03-02 22:07:05 -0500

    up

--- apps/devid_rodinia/hotspot/profile-this.sh
+++ apps/devid_rodinia/hotspot/profile-this.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-./hotspot 512 2 2 ../data/hotspot/temp_512 ../data/hotspot/power_512 output.out $1
+#./hotspot 512 2 2 ../data/hotspot/temp_512 ../data/hotspot/power_512 output.out $1
 
 # metrics
 nvprof --metrics all --csv     --log-file metrics_rodinia_hotspot.csv ./hotspot 512 2 2 ../data/hotspot/temp_512 ../data/hotspot/power_512 output.out 0 
