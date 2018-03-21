8510c582ce4e34bd6c7f29a0b1a2ca13d3eecc8a
Leiming Yu <leimingyu830@gmail.com>
2018-03-21 11:14:44 -0400

    fix htoa api error

---
+++ pycode/magus_util.py
@@ -0,0 +1,351 @@
+#!/usr/bin/env python
+
+import pandas as pd
+import numpy as np
+from math import *
+import operator
+import sys
+
+NVPROF_TRACE_COLS = [
+    "Start",
+    "Duration",
+    "Grid X",
+    "Grid Y",
+    "Grid Z",
+    "Block X",
+    "Block Y",
+    "Block Z",
+    "Registers Per Thread",
+    "Static SMem",
+    "Dynamic SMem",
+    "Size",
+    "Throughput",
+    "Device",
+    "Context",
+    "Stream",
+    "Name"]
+
+
+def sort_dict_by_val(inputDict):
+    """
+    Sort dictionary by the value
+    """
+    return sorted(inputDict.items(), key=operator.itemgetter(1))
+
+
+def read_nvprof_trace(traceFile):
+    df_trace = pd.read_csv(traceFile, names=NVPROF_TRACE_COLS, engine='python')
+    rows_to_skip = 0
+    # find out the number of rows to skip
+    for index, row in df_trace.iterrows():
+        if row['Start'] == 'Start':
+            rows_to_skip = index
+            break
+    # read the input csv again
+    df_trace = pd.read_csv(traceFile, skiprows=rows_to_skip)
+    return df_trace
+
+
+def parse_nvprof_trace(df_trace):
+    """
+    Read dataframe of the nvprof trace
+    Return a list of trace
+    """
+    #-----------------------------------
+    # start parsing the data frame
+    #-----------------------------------
+    start_coef, duration_coef = time_coef_ms(df_trace)  # time unit : ms
+    # print start_coef, duration_coef
+
+    trans_coef = trans_coef_kb(df_trace)  # normalize the transfer size to KB
+    # print trans_coef
+
+    ssm_coef, dsm_coef = sm_coef_bytes(df_trace)  # normalize the size to Bytes
+    # print ssm_coef, dsm_coef
+
+    #-----
+    # read each row for  api type, its timing and transfer size
+    #-----
+    appTrace = []
+    for rowID in xrange(1, df_trace.shape[0]):
+        # apiType, starT, endT, Grid, Block, Reg, SharedMem
+        CurrentApi = getRowInfo(
+            df_trace.iloc[[rowID]], start_coef, duration_coef, trans_coef, ssm_coef, dsm_coef)
+        # print CurrentApi
+        appTrace.append(CurrentApi)
+
+    return appTrace
+
+
+def getruntime(appTraceList):
+    """
+    Return the difference between 1st api start and last api end.
+    """
+    # print appTrace1[0][1], appTrace1[-1][2]
+    #print("app runtime (ms) : {}".format(appTms))
+    return appTraceList[-1][2] - appTraceList[0][1]
+
+
+class transfer():
+    def __init__(self, start=0.0, end=0.0, trans_size=0.0):
+        self.start_time_ms = start
+        self.end_time_ms = end
+        self.size = trans_size
+
+
+class streams():
+    def __init__(self):
+        self.h2d = []
+        self.d2h = []
+        self.d2d = []
+        self.memset = []
+        self.kernel = []
+        self.kernel_info = []
+
+#------------------------------------------------------------------------------
+# Use ms for timing
+#------------------------------------------------------------------------------
+def time_coef_ms(df_trace):
+    rows, cols = df_trace.shape
+
+    start_unit = df_trace['Start'].iloc[0]
+    duration_unit = df_trace['Duration'].iloc[0]
+
+    start_coef = 1.0
+    if start_unit == 's':
+        start_coef = 1e3
+    if start_unit == 'us':
+        start_coef = 1e-3
+
+    duration_coef = 1.0
+    if duration_unit == 's':
+        duration_coef = 1e3
+    if duration_unit == 'us':
+        duration_coef = 1e-3
+
+    return start_coef, duration_coef
+
+
+#------------------------------------------------------------------------------
+# Use bytes for shared memory
+#------------------------------------------------------------------------------
+def sm_coef_bytes(df_trace):
+    ssm_unit = df_trace['Static SMem'].iloc[0]
+    dsm_unit = df_trace['Dynamic SMem'].iloc[0]
+
+    ssm_coef = 1.0
+    if ssm_unit == 'KB':
+        ssm_coef = 1e3
+    if ssm_unit == 'MB':
+        ssm_coef = 1e6
+
+    dsm_coef = 1.0
+    if dsm_unit == 'KB':
+        dsm_coef = 1e3
+    if dsm_unit == 'MB':
+        dsm_coef = 1e6
+
+    return ssm_coef, dsm_coef
+
+
+#------------------------------------------------------------------------------
+# Use KB for data transfer
+#------------------------------------------------------------------------------
+def trans_coef_kb(df_trace):
+    size_unit = df_trace['Size'].iloc[0]
+
+    coef = 0.0  # KB
+
+    if size_unit == 'B':
+        coef = 1e-3
+    elif size_unit == 'MB':
+        coef = 1e3
+    elif size_unit == 'GB':
+        coef = 1e6
+    elif size_unit == 'KB':
+        coef = 1.0  # KB
+    else:
+        sys.stderr.write('Unknown Size Unit.\n')
+
+    return coef
+
+#------------------------------------------------------------------------------
+#  Read current row of the dataframe, return timing and transfer_size
+#------------------------------------------------------------------------------
+def getRowInfo(df_row, start_coef_ms, duration_coef_ms,
+               trans_coef, ssm_coef, dsm_coef):
+    """
+    Read the current row for the dataframe, extracting timing only.
+
+    :param df_row:              row of dataframe
+    :param start_coef_ms        start time coef in ms
+    :param duration_coef_m      duration time coef in ms
+
+    :return api_type:           api type : h2d, d2h, memset, kern
+    :return start_time_ms:      the starting time for current api
+    :return end_time_ms:        the ending time for current api
+    :return trans_kb:           the transfer size in KB
+    """
+    # parameters
+    api_name = df_row['Name'].to_string()
+    startT = float(df_row['Start']) * start_coef_ms
+    endT = startT + float(df_row['Duration']) * duration_coef_ms
+    tranSize = 0.0
+    Grid, Block, Reg, SharedMem = 0., 0., 0., 0.
+
+    if "DtoH" in api_name:
+        apiType = 'd2h'
+        tranSize = float(df_row.Size) * trans_coef  # d2h size in KB
+    elif "HtoD" in api_name or "HtoA" in api_name:   
+        # NOTE: cudaMemcpyToArray = HtoA
+        apiType = 'h2d'
+        tranSize = float(df_row.Size) * trans_coef  # h2d size in KB
+    elif "DtoD" in api_name:
+        apiType = 'd2d'
+        tranSize = float(df_row.Size) * trans_coef  # d2d size in KB
+    elif "memset" in api_name:
+        apiType = 'memset'
+        tranSize = 0      # no transfer over pcie
+    else:
+        apiType = 'kern'
+        Grid = float(df_row['Grid X']) * \
+            float(df_row['Grid Y']) * float(df_row['Grid Z'])
+        Block = float(df_row['Block X']) * \
+            float(df_row['Block Y']) * float(df_row['Block Z'])
+        Reg = float(df_row['Registers Per Thread'])
+        SharedMem = float(df_row['Static SMem']) * ssm_coef + \
+            float(df_row['Dynamic SMem']) * dsm_coef
+        # print Grid, Block, Reg, SharedMem
+
+    CurrentApi = [apiType, startT, endT, Grid, Block, Reg, SharedMem]
+    return CurrentApi
+
+
+#------------------------------------------------------------------------------
+# Generate Features for NN 
+#------------------------------------------------------------------------------
+
+# as default, take the 1st 5 api calls , if less than 5, fill in the blanks with 0., 
+# if more than 5 calls, leave the others alone
+
+# h2d: 1, d2h:2, d2d : 3 , memset 4, kern: 5
+# for each api call:  [apitype, startT, endT, A, B, C, D]
+# for transfer apis, A-D are zeros
+# for kernel, A = gridsize, B = blocksize, C = register usage, D =  shared memory usage
+# Therefore, there are 7 floats for each API
+
+# Since we use 5 (as default) Api call, we need to preallocate 35 floats per application
+
+# Besides, we include 10 feats : 
+# 1) app startT, 2) endT, 3) appDur, 
+# 4) kernel Calls 5) trans calls 
+# 6) kerns_time_ratio 7) trans_time_ratio
+# 8) avgKernsTime 9) avgTransTime 
+# 10) avgApiGap
+
+# Sum up, there are 45 features per applications
+
+def genNNFeat(app_trace, ApiNum = 5):
+    # pre-allocate array
+    feat_num = ApiNum * 7 + 10
+    feat_array = np.zeros(feat_num, dtype=float)
+    
+    transT = 0.
+    kernsT = 0.
+    
+    # 
+    startT, endT = app_trace[0][1], app_trace[-1][2]
+    appDur = endT - startT
+    
+    i = 0
+    gapSum = 0.
+    
+    transCalls = 0.
+    kernelCalls = 0.
+
+    READ_TRACE = True
+    for app in app_trace:
+        if app[0] in ['h2d', 'd2h', 'd2d', 'memset']:
+            transT = transT + (app[2] - app[1])
+            transCalls = transCalls + 1.
+            if READ_TRACE:
+                startpos = i * 7
+                
+                # e.g., ['h2d', 566.138617, 566.141849, 0.0, 0.0, 0.0, 0.0]
+                if app[0] == 'h2d':
+                    app_type_idx = 1
+                elif app[0] == 'd2h':
+                    app_type_idx = 2
+                elif app[0] == 'd2d':
+                    app_type_idx = 3
+                elif app[0] == 'memset':
+                    app_type_idx = 4
+                else:
+                    print("Error! Unknow transfer types!")
+                    sys.exit(1)
+                    
+                    
+                feat_array[startpos] = app_type_idx
+                feat_array[startpos + 1] = app[1] 
+                feat_array[startpos + 2] = app[2] 
+                feat_array[startpos + 3] = app[3] 
+                feat_array[startpos + 4] = app[4] 
+                feat_array[startpos + 5] = app[5] 
+                feat_array[startpos + 6] = app[6] 
+
+                #print feat_array[startpos], feat_array[startpos+1], feat_array[startpos+2], feat_array[startpos+3], feat_array[startpos+4], feat_array[startpos+5], feat_array[startpos+6]
+                
+
+        if app[0] == "kern":
+            app_type_idx = 5
+            kernsT = kernsT + (app[2] - app[1])
+            kernelCalls = kernelCalls + 1.
+            
+            if READ_TRACE:
+                startpos = i * 7
+                feat_array[startpos] = app_type_idx
+                feat_array[startpos + 1] = app[1] 
+                feat_array[startpos + 2] = app[2] 
+                feat_array[startpos + 3] = app[3] 
+                feat_array[startpos + 4] = app[4] 
+                feat_array[startpos + 5] = app[5] 
+                feat_array[startpos + 6] = app[6] 
+                
+                #print feat_array[startpos], feat_array[startpos+1], feat_array[startpos+2], feat_array[startpos+3], feat_array[startpos+4], feat_array[startpos+5], feat_array[startpos+6]
+        
+        if i>0:
+            gapSum = gapSum + (app_trace[i][1] - app_trace[i-1][2])
+            
+        i = i + 1
+        
+        if i > ApiNum:
+            READ_TRACE = False
+    
+    kerns_time_ratio = kernsT / appDur   # sum(kernels_runtime) / app_runtime
+    trans_time_ratio = transT / appDur   # sum(transfer_runtime) / app_runtime
+    
+    avgKernsTime = kernsT / kernelCalls
+    avgTransTime = transT / transCalls
+    
+    # avg interval between two consecutive apis
+    avgApiGap = gapSum / (len(app_trace) - 1)
+    
+    
+    # add the rest
+    startpos = ApiNum * 7
+    feat_array[startpos]     = startT
+    feat_array[startpos + 1] = endT
+    feat_array[startpos + 2] = appDur
+    feat_array[startpos + 3] = kernelCalls
+    feat_array[startpos + 4] = transCalls
+    feat_array[startpos + 5] = kerns_time_ratio
+    feat_array[startpos + 6] = trans_time_ratio
+    feat_array[startpos + 7] = avgKernsTime
+    feat_array[startpos + 8] = avgTransTime
+    feat_array[startpos + 9] = avgApiGap
+    
+    #print feat_array[startpos], feat_array[startpos+1], feat_array[startpos+2], feat_array[startpos+3], feat_array[startpos+4], feat_array[startpos+5], feat_array[startpos+6]
+
+    #print feat_array
+
+    return feat_array
