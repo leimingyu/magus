bf27aa158e8b73104093dbd36af4c7d2b8d1888f
Leiming Yu <leimingyu830@gmail.com>
2018-04-29 09:35:33 -0400

    79 app

--- 10_eval/prepare/save2json_79.py
+++ 10_eval/prepare/save2json_79.py
@@ -14,197 +14,197 @@ appList = []
 # cuda sdk 8.0:  ``$./run.sh devid``
 #------------------------------------------------------------------------------
 appList.append(GPUApp_pb2.GPUApp(
-    name='cudasdk-matrixMul',
+    name='cudasdk_matrixMul',
     dir='../apps/devid_cudasdk80/0_Simple/matrixMul',
     cmd='./run.sh'))
 
 appList.append(GPUApp_pb2.GPUApp(
-    name='cudasdk-vectorAdd',
+    name='cudasdk_vectorAdd',
     dir='../apps/devid_cudasdk80/0_Simple/vectorAdd',
     cmd='./run.sh'))
 
 appList.append(GPUApp_pb2.GPUApp(
-    name='cudasdk-convolutionFFT2D',
+    name='cudasdk_convolutionFFT2D',
     dir='../apps/devid_cudasdk80/3_Imaging/convolutionFFT2D',
     cmd='./run.sh'))
 
 appList.append(GPUApp_pb2.GPUApp(
-    name='cudasdk-convolutionSeparable',
+    name='cudasdk_convolutionSeparable',
     dir='../apps/devid_cudasdk80/3_Imaging/convolutionSeparable',
     cmd='./run.sh'))
 
 appList.append(GPUApp_pb2.GPUApp(
-    name='cudasdk-convolutionTexture',
+    name='cudasdk_convolutionTexture',
     dir='../apps/devid_cudasdk80/3_Imaging/convolutionTexture',
     cmd='./run.sh'))
 
 appList.append(GPUApp_pb2.GPUApp(
-    name='cudasdk-dct8x8',
+    name='cudasdk_dct8x8',
     dir='../apps/devid_cudasdk80/3_Imaging/dct8x8',
     cmd='./run.sh'))
 
 appList.append(GPUApp_pb2.GPUApp(
-    name='cudasdk-dwtHaar1D',
+    name='cudasdk_dwtHaar1D',
     dir='../apps/devid_cudasdk80/3_Imaging/dwtHaar1D',
     cmd='./run.sh'))
 
 appList.append(GPUApp_pb2.GPUApp(
-    name='cudasdk-dxtc',
+    name='cudasdk_dxtc',
     dir='../apps/devid_cudasdk80/3_Imaging/dxtc',
     cmd='./run.sh'))
 
 appList.append(GPUApp_pb2.GPUApp(
-    name='cudasdk-stereoDisparity',
+    name='cudasdk_stereoDisparity',
     dir='../apps/devid_cudasdk80/3_Imaging/stereoDisparity',
     cmd='./run.sh'))
 
 appList.append(GPUApp_pb2.GPUApp(
-    name='cudasdk-binomialOptions',
+    name='cudasdk_binomialOptions',
     dir='../apps/devid_cudasdk80/4_Finance/binomialOptions',
     cmd='./run.sh'))
 
 appList.append(GPUApp_pb2.GPUApp(
-    name='cudasdk-BlackScholes',
+    name='cudasdk_BlackScholes',
     dir='../apps/devid_cudasdk80/4_Finance/BlackScholes',
     cmd='./run.sh'))
 
 appList.append(GPUApp_pb2.GPUApp(
-    name='cudasdk-quasirandomGenerator',
+    name='cudasdk_quasirandomGenerator',
     dir='../apps/devid_cudasdk80/4_Finance/quasirandomGenerator',
     cmd='./run.sh'))
 
 appList.append(GPUApp_pb2.GPUApp(
-    name='cudasdk-SobolQRNG',
+    name='cudasdk_SobolQRNG',
     dir='../apps/devid_cudasdk80/4_Finance/SobolQRNG',
     cmd='./run.sh'))
 
 appList.append(GPUApp_pb2.GPUApp(
-    name='cudasdk-c++11_cuda',
+    name='cudasdk_c++11Cuda',
     dir='../apps/devid_cudasdk80/6_Advanced/c++11_cuda',
     cmd='./run.sh'))
 
 appList.append(GPUApp_pb2.GPUApp(
-    name='cudasdk-concurrentKernels',
+    name='cudasdk_concurrentKernels',
     dir='../apps/devid_cudasdk80/6_Advanced/concurrentKernels',
     cmd='./run.sh'))
 
 appList.append(GPUApp_pb2.GPUApp(
-    name='cudasdk-eigenvalues',
+    name='cudasdk_eigenvalues',
     dir='../apps/devid_cudasdk80/6_Advanced/eigenvalues',
     cmd='./run.sh'))
 
 appList.append(GPUApp_pb2.GPUApp(
-    name='cudasdk-fastWalshTransform',
+    name='cudasdk_fastWalshTransform',
     dir='../apps/devid_cudasdk80/6_Advanced/fastWalshTransform',
     cmd='./run.sh'))
 
 appList.append(GPUApp_pb2.GPUApp(
-    name='cudasdk-FDTD3d',
+    name='cudasdk_FDTD3d',
     dir='../apps/devid_cudasdk80/6_Advanced/FDTD3d',
     cmd='./run.sh'))
 
 appList.append(GPUApp_pb2.GPUApp(
-    name='cudasdk-interval',
+    name='cudasdk_interval',
     dir='../apps/devid_cudasdk80/6_Advanced/interval',
     cmd='./run.sh'))
 
 appList.append(GPUApp_pb2.GPUApp(
-    name='cudasdk-lineOfSight',
+    name='cudasdk_lineOfSight',
     dir='../apps/devid_cudasdk80/6_Advanced/lineOfSight',
     cmd='./run.sh'))
 
 appList.append(GPUApp_pb2.GPUApp(
-    name='cudasdk-mergeSort',
+    name='cudasdk_mergeSort',
     dir='../apps/devid_cudasdk80/6_Advanced/mergeSort',
     cmd='./run.sh'))
 
 appList.append(GPUApp_pb2.GPUApp(
-    name='cudasdk-radixSortThrust',
+    name='cudasdk_radixSortThrust',
     dir='../apps/devid_cudasdk80/6_Advanced/radixSortThrust',
     cmd='./run.sh'))
 
 appList.append(GPUApp_pb2.GPUApp(
-    name='cudasdk-reduction',
+    name='cudasdk_reduction',
     dir='../apps/devid_cudasdk80/6_Advanced/reduction',
     cmd='./run.sh'))
 
 appList.append(GPUApp_pb2.GPUApp(
-    name='cudasdk-scalarProd',
+    name='cudasdk_scalarProd',
     dir='../apps/devid_cudasdk80/6_Advanced/scalarProd',
     cmd='./run.sh'))
 
 appList.append(GPUApp_pb2.GPUApp(
-    name='cudasdk-scan',
+    name='cudasdk_scan',
     dir='../apps/devid_cudasdk80/6_Advanced/scan',
     cmd='./run.sh'))
 
 appList.append(GPUApp_pb2.GPUApp(
-    name='cudasdk-segmentationTreeThrust',
+    name='cudasdk_segmentationTreeThrust',
     dir='../apps/devid_cudasdk80/6_Advanced/segmentationTreeThrust',
     cmd='./run.sh'))
 
 appList.append(GPUApp_pb2.GPUApp(
-    name='cudasdk-shfl_scan',
+    name='cudasdk_shflscan',
     dir='../apps/devid_cudasdk80/6_Advanced/shfl_scan',
     cmd='./run.sh'))
 
 appList.append(GPUApp_pb2.GPUApp(
-    name='cudasdk-sortingNetworks',
+    name='cudasdk_sortingNetworks',
     dir='../apps/devid_cudasdk80/6_Advanced/sortingNetworks',
     cmd='./run.sh'))
 
 appList.append(GPUApp_pb2.GPUApp(
-    name='cudasdk-threadFenceReduction',
+    name='cudasdk_threadFenceReduction',
     dir='../apps/devid_cudasdk80/6_Advanced/threadFenceReduction',
     cmd='./run.sh'))
 
 appList.append(GPUApp_pb2.GPUApp(
-    name='cudasdk-transpose',
+    name='cudasdk_transpose',
     dir='../apps/devid_cudasdk80/6_Advanced/transpose',
     cmd='./run.sh'))
 
 appList.append(GPUApp_pb2.GPUApp(
-    name='cudasdk-batchCUBLAS',
+    name='cudasdk_batchCUBLAS',
     dir='../apps/devid_cudasdk80/7_CUDALibraries/batchCUBLAS',
     cmd='./run.sh'))
 
 appList.append(GPUApp_pb2.GPUApp(
-    name='cudasdk-boxFilterNPP',
+    name='cudasdk_boxFilterNPP',
     dir='../apps/devid_cudasdk80/7_CUDALibraries/boxFilterNPP',
     cmd='./run.sh'))
 
 appList.append(GPUApp_pb2.GPUApp(
-    name='cudasdk-MC_EstimatePiInlineP',
+    name='cudasdk_MC_EstimatePiInlineP',
     dir='../apps/devid_cudasdk80/7_CUDALibraries/MC_EstimatePiInlineP',
     cmd='./run.sh'))
 
 appList.append(GPUApp_pb2.GPUApp(
-    name='cudasdk-MC_EstimatePiInlineQ',
+    name='cudasdk_MC_EstimatePiInlineQ',
     dir='../apps/devid_cudasdk80/7_CUDALibraries/MC_EstimatePiInlineQ',
     cmd='./run.sh'))
 
 appList.append(GPUApp_pb2.GPUApp(
-    name='cudasdk-MC_EstimatePiP',
+    name='cudasdk_MC_EstimatePiP',
     dir='../apps/devid_cudasdk80/7_CUDALibraries/MC_EstimatePiP',
     cmd='./run.sh'))
 
 appList.append(GPUApp_pb2.GPUApp(
-    name='cudasdk-MC_EstimatePiQ',
+    name='cudasdk_MC_EstimatePiQ',
     dir='../apps/devid_cudasdk80/7_CUDALibraries/MC_EstimatePiQ',
     cmd='./run.sh'))
 
 appList.append(GPUApp_pb2.GPUApp(
-    name='cudasdk-MC_SingleAsianOptionP',
+    name='cudasdk_MC_SingleAsianOptionP',
     dir='../apps/devid_cudasdk80/7_CUDALibraries/MC_SingleAsianOptionP',
     cmd='./run.sh'))
 
 appList.append(GPUApp_pb2.GPUApp(
-    name='cudasdk-simpleCUBLAS',
+    name='cudasdk_simpleCUBLAS',
     dir='../apps/devid_cudasdk80/7_CUDALibraries/simpleCUBLAS',
     cmd='./run.sh'))
 
 appList.append(GPUApp_pb2.GPUApp(
-    name='cudasdk-simpleCUFFT_callback',
+    name='cudasdk_simpleCUFFTcallback',
     dir='../apps/devid_cudasdk80/7_CUDALibraries/simpleCUFFT_callback',
     cmd='./run.sh'))
 
@@ -220,67 +220,67 @@ appList.append(GPUApp_pb2.GPUApp(
 # polybench 
 #------------------------------------------------------------------------------
 appList.append(GPUApp_pb2.GPUApp(
-    name='poly-2DCONV',
+    name='poly_2dconv',
     dir='../apps/devid_poly/CUDA/2DCONV',
     cmd='./run.sh'))
 
 appList.append(GPUApp_pb2.GPUApp(
-    name='poly-3DCONV',
+    name='poly_3dconv',
     dir='../apps/devid_poly/CUDA/3DCONV',
     cmd='./run.sh'))
 
 appList.append(GPUApp_pb2.GPUApp(
-    name='poly-3MM',
+    name='poly_3mm',
     dir='../apps/devid_poly/CUDA/3MM',
     cmd='./run.sh'))
 
 appList.append(GPUApp_pb2.GPUApp(
-    name='poly-ATAX',
+    name='poly_atax',
     dir='../apps/devid_poly/CUDA/ATAX',
     cmd='./run.sh'))
 
 appList.append(GPUApp_pb2.GPUApp(
-    name='poly-BICG',
+    name='poly_bicg',
     dir='../apps/devid_poly/CUDA/BICG',
     cmd='./run.sh'))
 
 appList.append(GPUApp_pb2.GPUApp(
-    name='poly-CORR',
+    name='poly_correlation',
     dir='../apps/devid_poly/CUDA/CORR',
     cmd='./run.sh'))
 
 appList.append(GPUApp_pb2.GPUApp(
-    name='poly-COVAR',
+    name='poly_covariance',
     dir='../apps/devid_poly/CUDA/COVAR',
     cmd='./run.sh'))
 
 appList.append(GPUApp_pb2.GPUApp(
-    name='poly-FDTD-2D',
+    name='poly_fdtd2d',
     dir='../apps/devid_poly/CUDA/FDTD-2D',
     cmd='./run.sh'))
 
 appList.append(GPUApp_pb2.GPUApp(
-    name='poly-GEMM',
+    name='poly_gemm',
     dir='../apps/devid_poly/CUDA/GEMM',
     cmd='./run.sh'))
 
 appList.append(GPUApp_pb2.GPUApp(
-    name='poly-GESUMMV',
+    name='poly_gesummv',
     dir='../apps/devid_poly/CUDA/GESUMMV',
     cmd='./run.sh'))
 
 appList.append(GPUApp_pb2.GPUApp(
-    name='poly-MVT',
+    name='poly_mvt',
     dir='../apps/devid_poly/CUDA/MVT',
     cmd='./run.sh'))
 
 appList.append(GPUApp_pb2.GPUApp(
-    name='poly-SYR2K',
+    name='poly_syr2k',
     dir='../apps/devid_poly/CUDA/SYR2K',
     cmd='./run.sh'))
 
 appList.append(GPUApp_pb2.GPUApp(
-    name='poly-SYRK',
+    name='poly_syrk',
     dir='../apps/devid_poly/CUDA/SYRK',
     cmd='./run.sh'))
 
@@ -289,22 +289,22 @@ appList.append(GPUApp_pb2.GPUApp(
 # lonestar 
 #------------------------------------------------------------------------------
 appList.append(GPUApp_pb2.GPUApp(
-    name='lonestar-bh',
+    name='lonestar_bh',
     dir='../apps/devid_lonestar/apps/bh',
     cmd='./run.sh'))
 
 appList.append(GPUApp_pb2.GPUApp(
-    name='lonestar-dmr',
+    name='lonestar_dmr',
     dir='../apps/devid_lonestar/apps/dmr',
     cmd='./run.sh'))
 
 appList.append(GPUApp_pb2.GPUApp(
-    name='lonestar-mst',
+    name='lonestar_mst',
     dir='../apps/devid_lonestar/apps/mst',
     cmd='./run.sh'))
 
 appList.append(GPUApp_pb2.GPUApp(
-    name='lonestar-sssp',
+    name='lonestar_sssp',
     dir='../apps/devid_lonestar/apps/sssp',
     cmd='./run.sh'))
 
@@ -312,32 +312,32 @@ appList.append(GPUApp_pb2.GPUApp(
 # parboil 
 #------------------------------------------------------------------------------
 appList.append(GPUApp_pb2.GPUApp(
-    name='parboil-bfs',
+    name='parboil_bfs',
     dir='../apps/devid_parboil/benchmarks/bfs',
     cmd='./run.sh'))
 
 appList.append(GPUApp_pb2.GPUApp(
-    name='parboil-cutcp',
+    name='parboil_cutcp',
     dir='../apps/devid_parboil/benchmarks/cutcp',
     cmd='./run.sh'))
 
 appList.append(GPUApp_pb2.GPUApp(
-    name='parboil-lbm',
+    name='parboil_lbm',
     dir='../apps/devid_parboil/benchmarks/lbm',
     cmd='./run.sh'))
 
 appList.append(GPUApp_pb2.GPUApp(
-    name='parboil-mri-q',
+    name='parboil_mriq',
     dir='../apps/devid_parboil/benchmarks/mri-q',
     cmd='./run.sh'))
 
 appList.append(GPUApp_pb2.GPUApp(
-    name='parboil-sgemm',
+    name='parboil_sgemm',
     dir='../apps/devid_parboil/benchmarks/sgemm',
     cmd='./run.sh'))
 
 appList.append(GPUApp_pb2.GPUApp(
-    name='parboil-stencil',
+    name='parboil_stencil',
     dir='../apps/devid_parboil/benchmarks/stencil',
     cmd='./run.sh'))
 
@@ -345,22 +345,22 @@ appList.append(GPUApp_pb2.GPUApp(
 # rodinia
 #------------------------------------------------------------------------------
 appList.append(GPUApp_pb2.GPUApp(
-    name='rodinia-backprop',
+    name='rodinia_backprop',
     dir='../apps/devid_rodinia/backprop',
     cmd='./run.sh'))
 
 appList.append(GPUApp_pb2.GPUApp(
-    name='rodinia-b+tree',
+    name='rodinia_b+tree',
     dir='../apps/devid_rodinia/b+tree',
     cmd='./run.sh'))
 
 appList.append(GPUApp_pb2.GPUApp(
-    name='rodinia-dwt2d',
+    name='rodinia_dwt2d',
     dir='../apps/devid_rodinia/dwt2d',
     cmd='./run.sh'))
 
 appList.append(GPUApp_pb2.GPUApp(
-    name='rodinia-gaussian',
+    name='rodinia_gaussian',
     dir='../apps/devid_rodinia/gaussian',
     cmd='./run.sh'))
 
@@ -380,22 +380,22 @@ appList.append(GPUApp_pb2.GPUApp(
     cmd='./run.sh'))
 
 appList.append(GPUApp_pb2.GPUApp(
-    name='rodinia-lud',
+    name='rodinia_lud',
     dir='../apps/devid_rodinia/lud',
     cmd='./run.sh'))
 
 appList.append(GPUApp_pb2.GPUApp(
-    name='rodinia-lavaMD',
+    name='rodinia_lavaMD',
     dir='../apps/devid_rodinia/lavaMD',
     cmd='./run.sh'))
 
 appList.append(GPUApp_pb2.GPUApp(
-    name='rodinia-needle',
+    name='rodinia_needle',
     dir='../apps/devid_rodinia/nw',
     cmd='./run.sh'))
 
 appList.append(GPUApp_pb2.GPUApp(
-    name='rodinia-pathfinder',
+    name='rodinia_pathfinder',
     dir='../apps/devid_rodinia/pathfinder',
     cmd='./run.sh'))
 
